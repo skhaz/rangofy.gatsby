36ed2a93f745e40f7ec17e0d940ead87e2078b03
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-06-06 16:52:04 -0300

    Bump

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,35 @@
 module.exports = {
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-material-ui`,
     {
-      resolve: "gatsby-source-graphql",
+      resolve: `gatsby-plugin-web-font-loader`,
+      options: {
+        google: {
+          families: ["Roboto:300,400,500", "Material+Icons"],
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-graphql`,
       options: {
         fieldName: "remote",
         typeName: "Place",
         url: process.env.GATSBY_PLACES_GQL,
       },
     },
-    "gatsby-plugin-netlify",
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Kioskify",
+        short_name: "Kioskify",
+        start_url: "/",
+        background_color: "#51317c",
+        theme_color: "#51317c",
+        display: "standalone",
+      },
+    },
   ],
 }
