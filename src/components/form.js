8b66c4048484f63fa24956ae555d2b3a9b509949
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-06-19 15:07:53 -0300

    Better form

--- src/components/form.js
+++ src/components/form.js
@@ -1,108 +1,96 @@
-import React, { useCallback } from "react"
-import { makeStyles } from "@material-ui/core/styles"
+import React from "react"
 import AddIcon from "@material-ui/icons/Add"
 import Button from "@material-ui/core/Button"
-import Checkbox from "@material-ui/core/Checkbox"
 import Fab from "@material-ui/core/Fab"
-import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Paper from "@material-ui/core/Paper"
-import TextField from "@material-ui/core/TextField"
-import { Formik, Form, Field, FieldArray } from 'formik';
-import { useDocumentOnce } from "react-firebase-hooks/firestore"
+import { Formik, Form, FieldArray } from "formik"
+import { FormikTextField } from "formik-material-fields"
+import { useCollectionOnce } from "react-firebase-hooks/firestore"
 import * as Yup from "yup"
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    margin: theme.spacing(4),
-  },
+const validationSchema = Yup.object({})
 
-  paper: {
-    flexDirection: "column",
-    alignItems: "center",
-    padding: theme.spacing(2),
-    margin: theme.spacing(2),
-  },
+const Root = ({ children }) => (
+  <div
+    style={{
+      flexGrow: 1,
+    }}
+  >
+    {children}
+  </div>
+)
 
-  fab: {
-    position: "fixed",
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-}))
+const Wrapper = ({ children }) => (
+  <Paper
+    style={{
+      flexDirection: "column",
+      alignItems: "center",
+      padding: 16,
+      margin: 16,
+    }}
+  >
+    {children}
+  </Paper>
+)
 
-const validationSchema = Yup.object({
-  name: Yup.string().required("name is required"),
-  whatsapp: Yup.boolean().default(false),
-})
+const PlaceEntry = ({ place }) => {
+  return (
+    <FormikTextField
+      fullWidth
+      name={`${place}.name`}
+      label="Name"
+      margin="normal"
+    />
+  )
+}
 
-const Root = props => <div className={props.className}>{props.children}</div>
-
-const Profile = props => {
-  const {
-    errors,
-    handleBlur,
-    handleChange,
-    handleSubmit,
-    isSubmitting,
-    values,
-    touched,
-  } = props
+const PlaceArray = props => {
+  const { values, isSubmitting, handleSubmit } = props
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        fullWidth
-        name="name"
-        label="Name"
-        margin="normal"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.name}
+    <Form onSubmit={handleSubmit}>
+      <FieldArray
+        name="places"
+        render={() =>
+          values.places.map((_, index) => (
+            <Wrapper key={index}>
+              <PlaceEntry place={`places.${index}`} />
+            </Wrapper>
+          ))
+        }
       />
-      <TextField
-        fullWidth
-        name="mobile"
-        label="Mobile"
-        margin="normal"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.mobile}
-      />
-      <FormControlLabel
-        name="whatsapp"
-        label="WhatsApp?"
-        control={<Checkbox checked={values.whatsapp} />}
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
-      <Button
-        fullWidth
-        disabled={isSubmitting}
-        variant="contained"
-        color="primary"
-        type="submit"
-      >
-        Submit
-      </Button>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
-    </form>
+      <Wrapper>
+        <Button
+          fullWidth
+          disabled={isSubmitting}
+          variant="contained"
+          color="primary"
+          type="submit"
+        >
+          Submit
+        </Button>
+        <pre>{JSON.stringify(props, null, 2)}</pre>
+      </Wrapper>
+    </Form>
   )
 }
 
 export default ({ firestore }) => {
-  const classes = useStyles()
+  const collectionRef = firestore.collection("places")
 
-  const uid = "1jHeyVss8f8Ivwe4dtjB"
+  const [snapshot, loading, error] = useCollectionOnce(collectionRef)
 
-  const docRef = firestore.doc(`places/${uid}`)
+  const handleSubmit = ({ places }, { setSubmitting }) => {
+    const batch = firestore.batch()
 
-  const [document, loading, error] = useDocumentOnce(docRef)
+    places.forEach(place => {
+      const clone = { ...place }
+      const { id } = clone
+      delete clone.id
+      batch.set(firestore.doc(`places/${id}`), { ...clone }, { merge: true })
+    })
 
-  const handleSubmit = (values, { setSubmitting }) => {
-    const document = { ...values, published: false }
-
-    return docRef.update(document).then(() =>
+    return batch.commit().then(
       setTimeout(() => {
         setSubmitting(false)
       }, 300)
@@ -111,23 +99,30 @@ export default ({ firestore }) => {
 
   const handleClick = () => {}
 
-  const renderInner = () => (
-    <Paper className={classes.paper}>
-      <Formik
-        validationSchema={validationSchema}
-        initialValues={document.data()}
-        onSubmit={handleSubmit}
-        render={props => <Profile {...props} />}
-      />
-    </Paper>
-  )
+  const initialValues =
+    snapshot && snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
 
   return (
     <>
-      <Root className={classes.root}>
-        {!loading && !error && renderInner()}
+      <Root>
+        {!loading && !error && (
+          <Formik
+            validationSchema={validationSchema}
+            initialValues={{ places: initialValues }}
+            onSubmit={handleSubmit}
+            render={props => <PlaceArray {...props} />}
+          />
+        )}
       </Root>
-      <Fab color="secondary" className={classes.fab} onClick={handleClick}>
+      <Fab
+        color="secondary"
+        onClick={handleClick}
+        style={{
+          position: "fixed",
+          bottom: 20,
+          right: 20,
+        }}
+      >
         <AddIcon />
       </Fab>
     </>
