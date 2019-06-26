0c70968eb7eabd64925ebec36c6b2d82a86f6a1a
Rodrigo Delduca <rodrigodelduca@gmail.com>
2019-06-26 13:43:43 -0300

    Remove unused imports

--- src/components/container.js
+++ src/components/container.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { ThemeProvider } from "@material-ui/styles"
 import { createMuiTheme } from "@material-ui/core/styles"
-import { deepPurple, deepOrange } from "@material-ui/core/colors"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import Div100vh from "react-div-100vh"
 
