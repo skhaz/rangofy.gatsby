66e799aa2d5f0ea8404252b0fe38a8a5e5a5c1b3
Rodrigo Delduca <rodrigodelduca@gmail.com>
2020-12-14 21:16:15 -0300

    bump deps

--- src/components/map.js
+++ src/components/map.js
@@ -35,9 +35,8 @@ export default function Map({ options, onMount, className }) {
 
     const script = document.createElement("script")
     script.type = "text/javascript"
-    script.src =
-      `https://maps.google.com/maps/api/js?key=` +
-        process.env.GATSBY_MAPS_API_KEY
+    script.src = `https://maps.google.com/maps/api/js?key=` +
+      process.env.GATSBY_MAPS_API_KEY
 
     const headScript = document.getElementsByTagName("script")[0]
     headScript.parentNode.insertBefore(script, headScript)
